9fa7bc33f3fc3ec9d13a593015ee26bd8625c8d7
Edward <edwardphilip@gmail.com>
2021-09-22 14:54:20 -0400

    Finish working and refactored palindrome method

--- lib/ephl_palindrome.rb
+++ lib/ephl_palindrome.rb
@@ -2,7 +2,22 @@
 
 require_relative "ephl_palindrome/version"
 
-module EphlPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# module EphlPalindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+
+  private
+
+  def processed_content
+    scan(/[a-z]/i).join.to_s.downcase
+  end
+
 end
